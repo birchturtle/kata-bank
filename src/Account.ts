ecf85b8f83a28420fc3584dd215079d8abc15985
kj <kj@birchturtle.net>
2023-02-23 14:59:35 +0100

    add balance and transactions to accounts

--- src/Account.ts
+++ src/Account.ts
@@ -1,7 +1,34 @@
 export default class Account {
-    deposit(amount: number) {}
-    withdraw(amount: number) {}
+    private _balance: number;
+    private transactions: Transaction[];
+
+    constructor(balance: number) {
+        this._balance = balance;
+    }
+
+    deposit(amount: number) {
+        if (amount <= 0) {
+            throw new RangeError("Negative deposit amount");
+        }
+        this._balance += amount;
+    }
+    withdraw(amount: number) {
+        if (amount <= 0) {
+            throw new RangeError("Negative withdraw amount");
+        }
+        this._balance -= amount;
+    }
     printStatement(): string {
         return "";
     }
+
+    private logTransaction(t: Transaction) {
+        this.transactions.push(t);
+    }
+}
+
+class Transaction {
+    date: Date;
+    amount: number;
+    balance: number;
 }
--- test/Account.spec.ts
+++ test/Account.spec.ts
@@ -3,7 +3,7 @@ import assert = require("node:assert");
 
 describe("basic demands of account kata", () => {
     it("should respond to these three methods", () => {
-        let account = new Account();
+        let account = new Account(500);
         assert.equal(account.withdraw(10), undefined);
         assert.equal(account.deposit(10), undefined);
         assert.match(account.printStatement(), /.*/);
