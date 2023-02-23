771a09cee9756b118eef0d7178fb77bebdab76c7
kj <kj@birchturtle.net>
2023-02-23 15:07:48 +0100

    test withdrawals and balance

--- test/Account.spec.ts
+++ test/Account.spec.ts
@@ -2,10 +2,29 @@ import Account from "../src/Account";
 import assert = require("node:assert");
 
 describe("basic demands of account kata", () => {
+    let account = new Account(500.00);
+    it("should have correct starting balance", () => {
+        assert.equal(account.balance, 500.00);
+    })
+    
     it("should respond to these three methods", () => {
-        let account = new Account(500);
-        assert.equal(account.withdraw(10), undefined);
-        assert.equal(account.deposit(10), undefined);
+
+        assert.equal(account.withdraw(10.00), undefined);
+        assert.equal(account.deposit(10.00), undefined);
         assert.match(account.printStatement(), /.*/);
         })
+    
+    describe("Withdrawals", () => {
+
+        let account = new Account(500.00);
+
+        it("should withdraw correct amount from balance", () => {
+            assert.strictEqual(account.balance, 500.00);
+            const expectedBalanceAfterWithdraw = 300.00;
+
+            account.withdraw(200.00);
+
+            assert.strictEqual(account.balance, expectedBalanceAfterWithdraw);
+        })
+    })
 })
